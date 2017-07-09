071dc44050fcbd7839f98d5462311e64b7411596
Jakub Wojtyra <wojtyraj@gmail.com>
2017-07-09 15:24:13 +0000

    added prod script

---
+++ webpack.prod.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+var port = process.env.PORT || 8080;
+var host = process.env.IP || '127.0.0.1';
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'normalize.css',
+    './src/styles/app.scss',
+    './src/index'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname + '/public')
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      include: path.join(__dirname, '/src'),
+      query: {
+        presets: [ 'es2015', 'react', "stage-2"] 
+      }
+    }, {
+      test: /\.s?css$/,
+      loaders: ['style', 'css',"sass"],
+    }, {
+      test: /\.json$/,
+      loader: "json"
+    }]
+  },
+  resolve: {
+    root: path.resolve('./src')
+  },
+  devServer: {
+    port: port,
+    host: host
+  }
+};
