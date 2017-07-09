eed25fd4d1e1b7912f23c62f5b289dfcf3a7bf1f
Jakub Wojtyra <wojtyraj@gmail.com>
2017-07-09 19:47:05 +0000

    changed devtools

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'inline-source-map', 
+  devtool: 'source-map', 
   entry: [
     'normalize.css',
     './src/styles/app.scss',
