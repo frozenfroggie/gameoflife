a5e16e6c013b2596f9a47a3f0e94837ea41e3387
Jakub Wojtyra <wojtyraj@gmail.com>
2017-07-09 16:26:00 +0000

    ./..

--- server/auth/strategies/facebookAuth.js
+++ server/auth/strategies/facebookAuth.js
@@ -8,7 +8,7 @@ module.exports = function() {
     passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "https://enigmatic-island-38218.herokuapp.com/auth/facebook/callback"
+    callbackURL: "https://enigmatic-island-38218.herokuapp.com/auth/facebook/callback" 
   },
   function(accessToken, refreshToken, profile, cb) {
     FacebookAuthModel.findOrCreate({ facebookId: profile.id, profile: profile.displayName }, function (err, user) {
